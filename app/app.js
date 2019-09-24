f827fd851fc3494bac3311bda26bdc3d5cb1dce8
mingrui <474562318@qq.com>
2019-09-24 10:57:32 +0800

    chore: 增加 react-router-dom

--- app/app.js
+++ app/app.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
-import styles from './app.scss'
 
-const App = () => (
-  <div className={styles.container}>
-    <div>nav</div>
-    <div>body</div>
-    <div>footer123</div>
-  </div>
+const Home = () => {
+  return <h2>Home</h2>
+}
+
+const About = () => {
+  return <h2>About</h2>
+}
+
+const Users = () => {
+  return <h2>Users</h2>
+}
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/users'>Users</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <Route path='/' exact component={Home} />
+    <Route path='/about' exact component={About} />
+    <Route path='/users' exact component={Users} />
+  </BrowserRouter>
 )
 
-export default hot(App)
+export default hot(AppRouter)
